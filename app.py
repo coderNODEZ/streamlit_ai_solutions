6fecaa3804ed90f7c09f4dc91be8d3df3f7c3bb9
HR <rgxgxxxg@gmail.com>
2024-07-09 14:12:23 -0500

    Remove Streamlit Brand

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ from PIL import Image
 hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
